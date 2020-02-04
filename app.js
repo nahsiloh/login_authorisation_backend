88d23c48b0fd62da4906d54aca8f5bb4f3963624
nahsiloh <lishan1512@gmail.com>
2020-02-04 13:56:24 +0800

    installed cors and added corsOptions

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  preflightContinue: false,
+  credentials: true,
+  allowedHeaders: "content-type"
+};
+
+app.use(cors(corsOptions));
 
 if (app.get("env") !== "test") {
   require("./db");
