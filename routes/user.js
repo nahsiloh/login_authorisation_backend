4b3d12a8358a829cccd53d211334fcf0629e6e74
nahsiloh <lishan1512@gmail.com>
2020-02-04 13:56:46 +0800

    added bcrypt and jwt for password

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const User = require("../models/User");
+
+const JWT_SECRET_KEY = "Secret";
 
 router.get("/", async (req, res, next) => {
   try {
@@ -14,6 +19,11 @@ router.get("/", async (req, res, next) => {
 router.post("/new", async (req, res, next) => {
   try {
     const user = new User(req.body);
+
+    const rounds = 10;
+    const hash = await bcrypt.hash(user.password, rounds);
+    user.password = hash;
+
     await User.init();
     const newUser = await user.save();
     res.send(newUser);
@@ -23,6 +33,31 @@ router.post("/new", async (req, res, next) => {
     }
     next(err);
   }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      throw new Error(`${password}, ${user.password}`);
+    }
+
+    const token = jwt.sign({ name: user.username }, JWT_SECRET_KEY);
+    res.cookie("token", token);
+    res.send(user);
+  } catch (err) {
+    if (err.message === "Login failed") {
+      err.status = 400;
+    }
+    next(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  res.clearCookie("token").send("You are now logged out!");
 });
 
 module.exports = router;
