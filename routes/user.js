4ebfe10a997be26928fe50a7ac1c5ff430f83ef5
nahsiloh <lishan1512@gmail.com>
2020-02-06 16:12:04 +0800

    moved JWT_SECRET_KEY to .env file

--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,19 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
-const JWT_SECRET_KEY = "Secret";
-
 const authenticationController = async (req, res, next) => {
   try {
     if (!req.cookies.token) {
       throw new Error("Login to receive your secret message");
     }
-    req.user = jwt.verify(req.cookies.token, JWT_SECRET_KEY);
+    req.user = jwt.verify(req.cookies.token, process.env.JWT_SECRET_KEY);
     next();
   } catch (err) {
     res.status(401).end("You are not authorized!");
   }
 };
 
-router.get("/", authenticationController, async (req, res, next) => {
+router.get("/message", authenticationController, async (req, res, next) => {
   res.send("The Secret is Emporio Analytics");
 });
 
@@ -46,19 +44,20 @@ router.post("/login", async (req, res, next) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-
     const result = await bcrypt.compare(password, user.password);
+
     if (!result) {
-      throw new Error(`${password}, ${user.password}`);
+      throw new Error("Login failed");
     }
 
-    const token = jwt.sign({ name: user.username }, JWT_SECRET_KEY);
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.JWT_SECRET_KEY
+    );
     res.cookie("token", token);
     res.send(user);
   } catch (err) {
-    if (err.message === "Login failed") {
-      err.status = 400;
-    }
+    err.status = 400;
     next(err);
   }
 });
