82091c58e9c3e5f5943c07b90493eead776e9d5c
nahsiloh <lishan1512@gmail.com>
2020-02-06 16:35:17 +0800

    added dotenv package

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 app.use(cookieParser());
 app.use(express.json());
