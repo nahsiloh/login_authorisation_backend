3e566cff8019d7365290648b49407316406a3945
nahsiloh <lishan1512@gmail.com>
2020-02-06 16:10:31 +0800

    added bcrypt function

--- models/User.js
+++ models/User.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, index: true, unique: true },
   password: { type: String, required: true, minlength: 8 }
+});
+
+userSchema.pre("save", async function(next) {
+  const rounds = 10;
+  this.password = await bcrypt.hash(this.password, rounds);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
